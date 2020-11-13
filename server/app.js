089d537ec7581446cbfef24f0492e9e1c51b1c53
farisarmap <farisarmap@outlook.com>
2020-11-13 14:35:57 +0700

    server

--- server/app.js
+++ server/app.js
@@ -8,19 +8,37 @@ const io = require('socket.io')(http)
 app.use(cors())
 app.use(express.json())
 
-let scoreOne = 0
-let scoreTwo = 0
+let player1 = {
+    username: '',
+    score : 0
+}
+let player2 = {
+    username: '',
+    score: 0
+}
 io.on('connection',(socket)=>{
     console.log('User Connected')
-        socket.on('narik1', (score) => {
-            scoreOne = score
-            console.log(scoreOne);
-            io.emit('narik1', scoreOne)
+        socket.on('joinRoom', (payload) => {
+            // console.log(payload);
+            if (player1.username == '') {
+                player1.username = payload
+                console.log(payload, "<<< payload player1");
+                socket.emit('enteringRoomPlayer1', player1)
+            } else if (player2.username == '') {
+                player2.username = payload
+                console.log(payload, "<<< payload player 2");
+                socket.emit('enteringRoomPlayer2', player2)
+            }
         })
-
-        socket.on('narik2', (score) => {
-            scoreTwo = score
-            io.emit('narik2', scoreTwo)
+        socket.on('narikSocket', (data)=>{
+            console.log(data, "<<< socket narik 1 server");
+            if(data.role == 'Player 1') {
+                player1.score = data.score
+                io.emit('scorePlayer1', player1.score)
+            } else if (data.role == 'Player 2') {
+                player2.score = data.score
+                io.emit('scorePlayer2', player2.score)
+            }  
         })
 })
 
